fa660214decae3e56532990d73b80bbce4aacec7
ArcueidShiki <52120441+ArcueidShiki@users.noreply.github.com>
2024-08-28 16:38:16 +0800

    List steps, requirements and useful link
    Signed-off-by: ArcueidShiki <52120441+ArcueidShiki@users.noreply.github.com>

--- src/runml.c
+++ src/runml.c
@@ -34,6 +34,32 @@ int check_file(char *filepath)
     return fd;
 }
 
+/**
+ * Ref: https://www.toptal.com/scala/writing-an-interpreter
+ * TODO: Program runml should do the following:
+ * 1. Read each line of the file
+ * 2. Extract tokens from each line:
+ *      keywords[function, return, arg0, arg1, '\t'],
+ *      operands[x, y],
+ *      operators[<-, +, *, (, ), ',', #, whitespace],
+ *      variables[value, one],
+ *      functions[print, increment],
+ *      parameters[local para]
+ *      types[real, integer]
+ * 3. Check new line syntax based on previous line tokens
+ * 4. Write translated tokens to c code and format.
+ * 5. Validates and Report any errors commencing with the ! character. (Must detect all invalid ml programs)
+ * 6. Generate C11 code in a file name, for exmaple ml-$pid.c
+ * 7. Compile ml-$pid.c
+ * 8. Execute ./ml-$pid, passing any optional command-line arguments(real numbers)
+ * 9. Removes any files that it created.
+ *
+ * Requirements:
+ * All syntax errors detected in invalid ml programs must be reported via stderr on a line commencing with the '!' character. Your runml program must be able to detect all invalid ml programs - EXCEPT that your program will not be tested with any invalid expressions, so you do not need to validate the syntax of expressions.
+ * The only 'true' output produced by your translated and compiled program (when running) is the result of executing ml's print statement. Any 'debug' printing should appear on a line commencing with the '@' character.
+ * When printed, numbers that are exact integers must be printed without any decimal places; other numbers must be printed with exactly 6 decimal places.
+ */
+
 int main(int argc, char **argv)
 {
     check_args(argc, argv);
