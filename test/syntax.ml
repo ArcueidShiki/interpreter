ba1ceec1df5a8bb6e328eacdf89b864e93785e63
ArcueidShiki <52120441+ArcueidShiki@users.noreply.github.com>
2024-08-28 16:47:14 +0800

    Add ml syntax; init valid.ml and invalid.ml for testing to ensure covering all the possibilities
    Signed-off-by: ArcueidShiki <52120441+ArcueidShiki@users.noreply.github.com>

---
+++ test/syntax.ml
@@ -0,0 +1,37 @@
+  |       means a choice
+  [...]   means zero or one
+  (...)*  means zero or more
+
+Program lines commence at the left-hand margin (no indentation).
+Statements forming the body of a function are indented with a single tab.
+
+program:
+           ( program-item )*
+
+program-item:
+           statement
+        |  function identifier ( identifier )*
+           ←–tab–→ statement1
+           ←–tab–→ statement2
+           ....
+
+statement:
+           identifier "<-" expression
+        |  print  expression
+        |  return expression
+        |  functioncall
+
+expression:
+          term   [ ("+" | "-")  expression ]
+
+term:
+          factor [ ("*" | "/")  term ]
+
+factor:
+          realconstant
+        | identifier
+        | functioncall
+        | "(" expression ")"
+
+functioncall:
+          identifier "(" [ expression ( "," expression )* ] ")"
